d29fbfcd8df4f0b59479bbdd3ea436caad1785e1
Alexander Krechetov <krechet0v.alex@yandex.ru>
2026-07-31 13:49:22 +0300

    chore: clean unused Django files and configure admin

--- backend/equipment/admin.py
+++ backend/equipment/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Company, Equipment, SparePart
+
+
+class CompanyAdmin(admin.ModelAdmin):
+    pass
+
+
+class EquipmentAdmin(admin.ModelAdmin):
+    pass
+
+
+class SparePartAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(Company, CompanyAdmin)
+admin.site.register(Equipment, EquipmentAdmin)
+admin.site.register(SparePart, SparePartAdmin)
